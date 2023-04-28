76d37d82802f93bb994f418052dc52dcf0eb3553
safin777 <sayedislamsafin7770555@gmail.com>
2023-04-28 21:53:16 +0600

    Props in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./layouts/Nav"
 import TextForm from "./components/TextForm"
 import About from "./components/About"
+import Store from "./components/Store"
 const App = () => {
   return (
     <div className="App">
       <Navbar title="Dev-Labour" about="About Us" home="Home" />
-      <TextForm heading="Enter your text here" />
-      {/* <About /> */}
+      <Store />
     </div>
   )
 }
---
+++ src/components/Store.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Product from './Product'
+export default function Store() {
+  //array of objects of products
+  const products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'This is product 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'This is product 2',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      description: 'This is product 3',
+      price: 300,
+    },
+  ]
+
+  let productt = products.map((product) => {
+    return (
+    <Product key={product.id} name={product.name} description={product.description} price={product.price}/>)
+  })
+
+  return (
+    <div>
+      <div className="bg-gray-800 m-auto max-w-sm">
+        <h1 className="text-white text-3xl">Store</h1>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        {
+          productt
+        }
+      </div>
+    </div>
+  )
+}
